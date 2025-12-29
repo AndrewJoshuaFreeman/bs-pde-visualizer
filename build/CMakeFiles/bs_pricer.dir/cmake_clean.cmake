77555a89925d50505f20e76b983d012fea976a51
Andrew Freeman <151768859+PsuedoDataDynamo@users.noreply.github.com>
2025-12-29 17:00:31 -0600

    cmake and build files for OpenGL

---
+++ build/CMakeFiles/bs_pricer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bs_pricer.dir/main.cpp.o"
+  "CMakeFiles/bs_pricer.dir/main.cpp.o.d"
+  "bs_pricer"
+  "bs_pricer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bs_pricer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
